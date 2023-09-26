63b1d0e1626bbdc9aa43232abaf05dd9bc5162ae
Yuri <yuri.araujo-ext@tuibrasil.com>
2023-09-26 03:21:50 -0300

    chore: add migrations config

---
+++ knexfile.ts
@@ -0,0 +1,18 @@
+import type { Knex } from 'knex';
+import { dbConfig } from './src/database/config';
+
+const fullDbConfig = {
+  ...dbConfig,
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './src/database/migrations',
+  },
+};
+
+const config: { [key: string]: Knex.Config } = {
+  development: fullDbConfig,
+  staging: fullDbConfig,
+  production: fullDbConfig,
+};
+
+module.exports = config;
--- src/database/config.ts
+++ src/database/config.ts
@@ -1,21 +1,24 @@
 import knex, { Knex } from 'knex';
+import { DB_CLIENT } from '../common/constants';
 import dotenv from 'dotenv';
 dotenv.config();
+
+export const dbConfig = {
+  client: DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || ''),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+  pool: { min: 2, max: 10 },
+};
 
 let dbConnection: Knex;
 export default function getDBConnection() {
   if (!dbConnection) {
-    dbConnection = knex({
-      client: 'mysql',
-      debug: false,
-      connection: {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || ''),
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-      },
-    });
+    dbConnection = knex(dbConfig);
   }
 
   return dbConnection;
