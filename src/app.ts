692c784e0601fac26466b1384f716cab68dfbf01
Yuri <yuri.araujo-ext@tuibrasil.com>
2023-10-02 03:53:00 -0300

    chore: add ajv compiler to fastify instance

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,18 @@ import fastify from 'fastify';
 import userRouter from './components/users/entry-points/routes';
 import videoRouter from './components/videos/entry-points/routes';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import AJV from 'ajv';
+import addFormats from 'ajv-formats';
+import addErrors from 'ajv-errors';
+
+const ajv = new AJV({ allErrors: true });
+addFormats(ajv, ['email', 'time', 'uri'])
+  .addKeyword('kind')
+  .addKeyword('modifier');
+addErrors(ajv);
 
 const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
+server.setValidatorCompiler(({ schema }) => ajv.compile(schema));
 server.register(userRouter, { prefix: '/users' });
 server.register(videoRouter, { prefix: '/videos' });
 
