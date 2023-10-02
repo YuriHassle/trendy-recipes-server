fb792ac62b4919c21042948acb1f75109dabb01b
Yuri <yuri.araujo-ext@tuibrasil.com>
2023-10-02 01:53:47 -0300

    chore: add fastify type provider

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import fastify from 'fastify';
 import userRouter from './components/users/entry-points/routes';
 import videoRouter from './components/videos/entry-points/routes';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 
-const server = fastify();
+const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
 server.register(userRouter, { prefix: '/users' });
 server.register(videoRouter, { prefix: '/videos' });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 3000, host: '127.0.0.1' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
