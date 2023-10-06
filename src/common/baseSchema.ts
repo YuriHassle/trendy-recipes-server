6963cf857e1e71c43b8ba3da82c96cd48be74fe4
Yuri <yuri.araujo-ext@tuibrasil.com>
2023-10-06 04:27:41 -0300

    refactor: add enum

--- src/common/baseSchema.ts
+++ src/common/baseSchema.ts
@@ -7,11 +7,16 @@ import {
 } from 'fastify';
 import { RouteGenericInterface } from 'fastify/types/route';
 
+enum OrderDirection {
+  asc = 'asc',
+  desc = 'desc',
+}
+
 export const BaseQuery = Type.Object({
   limit: Type.Optional(Type.String()),
   offset: Type.Optional(Type.String()),
   orderBy: Type.Optional(Type.String()),
-  orderDirection: Type.Optional(Type.String({ enum: ['asc', 'desc'] })),
+  orderDirection: Type.Optional(Type.Enum(OrderDirection)),
 });
 
 export const BaseParam = Type.Object({
