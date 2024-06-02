5ae48c5c020e4f8fa3a8cb2ce053ad28b78e2057
Yuri <yuri.araujo-ext@tuibrasil.com>
2024-06-02 19:30:02 -0300

    chore: test vercel config

---
+++ api/serverless.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Read the .env file.
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// Require the framework
+import Fastify from 'fastify';
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// // Register your application as a normal plugin.
+// app.register(import('../src/app.js'));
+
+app.get('/', async (req, res) => {
+  return res.status(200).type('text/html').send('hello world');
+});
+
+export default async (req, res) => {
+  console.log('Fastify Serverless');
+  await app.ready();
+  app.server.emit('request', req, res);
+};
